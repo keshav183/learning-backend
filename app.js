b23688cbbbcf0dbefd863cdc5d52d6f6cffa66d5
keshav-repo <keshavrohilla987@gmail.com>
2025-07-27 17:32:11 +0530

    linked html css js and form submitting post method

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.use((req,res,next)=>{
     return next();
 })
 app.use(morgan('dev'));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
 app.set('view engine' , 'ejs')
 app.get('/' ,  (req,res,next)=>{
     const a = 1;
@@ -16,6 +19,10 @@ app.get('/' ,  (req,res,next)=>{
  (req,res)=>{res.render("index")})
 app.get('/about' ,(req,res)=>{res.send("welcome about")} )
 app.get('/profile' , (req,res)=>{res.send("profile page")}) 
-
+app.post('/get-form-data', (req,res)=>{
+    console.log(req.body)
+    res.send('data received');
+})
 
 app.listen(3000)
+
